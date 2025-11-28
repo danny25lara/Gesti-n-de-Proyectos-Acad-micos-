911326651be8c7cf95eab1fcd1d03e32ed8e4735
danny25lara <angeldaniellara25@gmail.com>
2025-11-27 19:49:32 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,18 +5,22 @@ import { renderizarTabla } from './views/vistaReporte.js';
 const btnIngresar = document.getElementById('btnIngresarSistema');
 const pantallaBienvenida = document.getElementById('pantallaBienvenida');
 const aplicacionPrincipal = document.getElementById('aplicacionPrincipal');
+// Buscamos el elemento del cohete (el div con el emoji)
+const coheteHero = document.querySelector('.icono-hero');
 
 btnIngresar.addEventListener('click', function() {
-    // 1. Hacemos que la bienvenida se desvanezca
+    // 1. ¡Activar el despegue del cohete!
+    // Le añadimos la clase que tiene la animación loca
+    coheteHero.classList.add('cohete-despegando');
+    // 2. Desvanecer el fondo (el contenedor de bienvenida)
     pantallaBienvenida.classList.add('desaparecer');
-
-    // 2. Esperamos medio segundo (0.5s) a que termine la animación de desaparecer
-    // y luego mostramos la aplicación principal
+    // 3. Esperar a que termine la animación del cohete antes de cambiar de pantalla.
+    // La animación CSS dura 1.2 segundos (1200ms). Esperamos ese tiempo exacto.
     setTimeout(() => {
-        pantallaBienvenida.style.display = 'none'; // La quitamos del todo
+        pantallaBienvenida.style.display = 'none'; // Quitamos la bienvenida del todo
         aplicacionPrincipal.classList.remove('app-oculta');
-        aplicacionPrincipal.classList.add('mostrar-app-animado'); // Animación de entrada
-    }, 500);
+        aplicacionPrincipal.classList.add('mostrar-app-animado'); // Entra la app suavemente
+    }, 3000);
 });
 // Referencias a los elementos del menú al dar click en cada boton se ejecuta una función para cambiar la vista de registro a reporte y viceversa
 let btnReporte = document.getElementById('VerReporte');
@@ -75,4 +79,5 @@ if (btnImprimir) {
         // Abrir el panel de impresión del navegador
         window.print();
     });
+
 }
